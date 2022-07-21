0f60b55d04028c6662cde99c67f38ab812e2c919
kailong321200875 <321200875@qq.com>
2022-07-21 17:58:05 +0800

    wip: demo开发中

---
+++ src/views/pip/pip.js
@@ -0,0 +1,15 @@
+import './pip.less'
+import '@/styles/base.css'
+
+window.onload = function () {
+  const video = document.getElementsByTagName('video')[0]
+  // 开启画中画模式
+
+  // console.log(video)
+  // video.onload = function () {
+  //   console.log('video loaded')
+  //   if (document.pictureInPictureEnabled && video) {
+  //     video.requestPictureInPicture()
+  //   }
+  // }
+}
