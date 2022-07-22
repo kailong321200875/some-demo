589b905e00090223b49a3d29e30766aee67ba7fb
kailong321200875 <321200875@qq.com>
2022-07-22 17:48:05 +0800

    wip: 画中画

--- src/views/pip/pip.js
+++ src/views/pip/pip.js
@@ -2,9 +2,8 @@ import './pip.less'
 import '@/styles/base.css'
 
 window.onload = function () {
-  const video = document.getElementsByTagName('video')[0]
+  // const video = document.getElementsByTagName('video')[0]
   // 开启画中画模式
-
   // console.log(video)
   // video.onload = function () {
   //   console.log('video loaded')
