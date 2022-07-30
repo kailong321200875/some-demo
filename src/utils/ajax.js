fff69b0988a5051705ffd932539c4aaff7ca7b45
kailong321200875 <321200875@qq.com>
2022-07-30 15:39:52 +0800

    feat: 添加jquery

---
+++ src/utils/ajax.js
@@ -0,0 +1,34 @@
+const urlPath =
+  process.env.NODE_ENV === 'development'
+    ? 'http://192.168.168.104:8324/'
+    : 'https://www.shoufacm.com/'
+
+const ajax = function (type, data, url, headersType) {
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: urlPath + url,
+      type: type || 'post',
+      dataType: 'JSON',
+      data: data,
+      timeout: 30000,
+      beforeSend: function (xhr) {
+        // 可以设置token
+        // if (token) {
+        //   xhr.setRequestHeader('token', token)
+        // }
+      },
+      processData: false,
+      contentType: false,
+      success: function (res) {
+        if (res.code === '0') {
+          resolve(res)
+        }
+      },
+      error: function (res) {
+        reject(res)
+      }
+    })
+  })
+}
+
+export default ajax
