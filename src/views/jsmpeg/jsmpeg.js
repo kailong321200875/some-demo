fdaaa88efa36205fdccdae0678348deb8ab51840
kailong321200875 <321200875@qq.com>
2023-04-25 09:33:45 +0800

    feat: jsmpeg

--- src/views/jsmpeg/jsmpeg.js
+++ src/views/jsmpeg/jsmpeg.js
@@ -1,2 +1,18 @@
 import './jsmpeg.less'
 import '@/styles/base.css'
+
+const url = 'https://sanyuanda.oss-cn-hangzhou.aliyuncs.com/some-demo/out.ts'
+
+window.onload = () => {
+  const player = new window.JSMpeg.Player(url, {
+    canvas: document.getElementById('canvas'),
+    autoplay: false,
+    loop: true,
+    // 关闭声音
+    audio: false,
+    onSourceEstablished: async () => {
+      player.play()
+      console.log('onSourceEstablished')
+    }
+  })
+}
